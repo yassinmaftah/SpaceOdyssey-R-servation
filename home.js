35efd3e19ef52d28ee6a99e0e1873f3785f84a18
yassinmftah <yassinmaftah06@gmail.com>
2025-11-14 11:44:05 +0100

    handel logout logic

--- home.js
+++ home.js
@@ -1,3 +1,29 @@
+function createStars() {
+            const container = document.getElementById('stars-container');
+            const starCount = 150;
+            
+            for (let i = 0; i < starCount; i++) {
+                const star = document.createElement('div');
+                star.classList.add('star');
+                
+                const size = Math.random() * 2 + 1;
+                star.style.width = `${size}px`;
+                star.style.height = `${size}px`;
+                
+                star.style.left = `${Math.random() * 100}%`;
+                star.style.top = `${Math.random() * 100}%`;
+                
+                star.style.animationDelay = `${Math.random() * 5}s`;
+                
+                container.appendChild(star);
+            }
+        }
+        
+        document.addEventListener('DOMContentLoaded', function() {
+            createStars();
+        });
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const desktopLoginLink = document.getElementById('desktop-login-link');
     const desktopUserStatus = document.getElementById('desktop-user-status');
